4cf9957139559d0e7b7f334d7b18f05880fb57e8
[ingdev2] <[ingdesarrollo2@bonnadona.co]>
2024-03-01 11:50:34 -0500

    nextjs project created

--- client/src/app/redux/store.ts
+++
@@ -1,6 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./slices/user/userSlice";
-
-export const store = configureStore({
-  reducer: { user: userReducer },
-});
