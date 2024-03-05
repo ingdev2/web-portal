e8d1ae1e7b6972b856aa4b49e61310ee0d085611
[ingdev2] <[ingdesarrollo2@bonnadona.co]>
2024-03-05 09:56:03 -0500

    auth provider nextjs

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Button } from "antd";
 import ButtonAuth from "@/components/ButtonAuth";
 
-const Home = () => (
+const HomePage = () => (
   <div className="App">
+    <h2>Página de Inicio</h2>
     <Button type="primary">Botón de Ant Design</Button>
+    <br />
     <br />
     <ButtonAuth></ButtonAuth>
   </div>
 );
 
-export default Home;
+export default HomePage;
